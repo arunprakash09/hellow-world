dd302f6ec92f3d9bb944855fc8089ccab59a5555
arunprakash09 <18f206@kce.ac.in>
2021-01-31 20:36:59 +0530

    First commit

---
+++ main.js
@@ -0,0 +1,7 @@
+function lightbulbOn() {
+    document.getElementById("lightbulb").src = "lightbulb-on.svg";
+  }
+  
+  function lightbulbOff() {
+    document.getElementById("lightbulb").src = "lightbulb-off.svg";
+  }
